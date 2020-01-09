e74a5f8c22e85122221952882afc20c0b412bfc1
Shane Geary <stg4462@comcast.net>
2020-01-09 18:52:29 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,4 +1,7 @@
 # code your #position_taken? method here!
 def position_taken?(board, idx)
-  
+  if board[idx] == nil
+    return false
+    elsif board[idx] == " " || board[idx] == ""
+    return false
 end
